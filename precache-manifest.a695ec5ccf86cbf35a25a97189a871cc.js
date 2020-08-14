c9e10cb0ac0eba87124b891a6038358b5f083643
neeshant <43741240+neeshant@users.noreply.github.com>
2020-08-14 15:13:45 +0530

    Updates

---
+++ precache-manifest.a695ec5ccf86cbf35a25a97189a871cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9223154201f3991e594c296c5431c990",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "8b0ead8e87bb170e01ad",
+    "url": "/robofriends/static/css/main.344c2ebc.chunk.css"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/robofriends/static/js/2.0b5b9267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.0b5b9267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b0ead8e87bb170e01ad",
+    "url": "/robofriends/static/js/main.91d8ddc4.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
